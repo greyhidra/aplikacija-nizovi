f1e6332921dd414736e24eebce859b812d8253cc
themi <themi@DESKTOP-SN334VG>
2024-09-18 13:09:32 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int duzina = int.Parse(Console.ReadLine());
+            int[] niz = new int[duzina];
+
+            for (int i = 0; i < niz.Length; i++)
+            {
+                // Console.WriteLine(i);
+                niz[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < niz.Length; i++)
+            {
+                if (niz[i] % 2 == 0)
+                {
+                    Console.WriteLine("Paran broj" + i + "je broj" + niz[i]);
+                }
+            }
+        }
+    }
+}
